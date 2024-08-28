62158946d1b1ea2556ce687e015abb72c6e59d98
JeronimohTV <j.marks@live.com>
2024-08-28 20:40:18 +0200

    Added a combo box to the form and populate it with the available audio device names.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             btnPlay = new Button();
+            btnListen = new Button();
+            comboBox1 = new ComboBox();
+            btnGetDevices = new Button();
             SuspendLayout();
             // 
             // btnPlay
             // 
-            btnPlay.Location = new Point(383, 161);
+            btnPlay.Location = new Point(40, 36);
             btnPlay.Name = "btnPlay";
             btnPlay.Size = new Size(124, 68);
             btnPlay.TabIndex = 0;
@@ -41,11 +44,42 @@
             btnPlay.UseVisualStyleBackColor = true;
             btnPlay.Click += btnPlay_Click;
             // 
+            // btnListen
+            // 
+            btnListen.Location = new Point(40, 280);
+            btnListen.Name = "btnListen";
+            btnListen.Size = new Size(124, 67);
+            btnListen.TabIndex = 1;
+            btnListen.Text = "Listen";
+            btnListen.UseVisualStyleBackColor = true;
+            btnListen.Click += btnListen_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(355, 59);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(166, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // btnGetDevices
+            // 
+            btnGetDevices.Location = new Point(253, 59);
+            btnGetDevices.Name = "btnGetDevices";
+            btnGetDevices.Size = new Size(75, 23);
+            btnGetDevices.TabIndex = 3;
+            btnGetDevices.Text = "Get Devices";
+            btnGetDevices.UseVisualStyleBackColor = true;
+            btnGetDevices.Click += btnGetDevices_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnGetDevices);
+            Controls.Add(comboBox1);
+            Controls.Add(btnListen);
             Controls.Add(btnPlay);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +89,8 @@
         #endregion
 
         private Button btnPlay;
+        private Button btnListen;
+        private ComboBox comboBox1;
+        private Button btnGetDevices;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,7 @@
 using System.Media;
+using NAudio;
+using NAudio.Wasapi;
+using NAudio.Wave;
 
 namespace TheAudioVisualiser
 
@@ -15,6 +18,22 @@ namespace TheAudioVisualiser
         {
             SoundPlayer splayer = new SoundPlayer(@"D:\04 Music Collection\Pr0xima - Carrier Phreakuency.wav");
             splayer.Play();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Adds current audio device names to the combo box.
+        private void btnGetDevices_Click(object sender, EventArgs e)
+        {
+            for (int n = -1; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                Console.WriteLine($"{n}: {caps.ProductName}");
+                comboBox1.Items.Add(caps.ProductName);
+            }
         }
     }
 }
