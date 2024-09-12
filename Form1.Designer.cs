43fc05c55a67606afdbd7fbab99de4993040cc56
JeronimohTV <j.marks@live.com>
2024-09-12 20:05:24 +0200

    Updated the project to finally use NAudio to create an audio loopback. The form now populates a selectable list view with the available devices.
    When the "Start" button, a stream is created (recorded) from the selected device and audio from that stream is played.
    
    This is called a loopback and if you select your mic as a device you will hear yourself through your audio device and potentially will hear some feedback too.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,20 @@
         private void InitializeComponent()
         {
             btnPlay = new Button();
-            btnListen = new Button();
             comboBox1 = new ComboBox();
             btnGetDevices = new Button();
+            splitContainer1 = new SplitContainer();
+            sourceList = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            btnExit = new Button();
+            btnStop = new Button();
+            btnStart = new Button();
+            btnSources = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // btnPlay
@@ -43,16 +54,6 @@
             btnPlay.Text = "Play Sound";
             btnPlay.UseVisualStyleBackColor = true;
             btnPlay.Click += btnPlay_Click;
-            // 
-            // btnListen
-            // 
-            btnListen.Location = new Point(40, 280);
-            btnListen.Name = "btnListen";
-            btnListen.Size = new Size(124, 67);
-            btnListen.TabIndex = 1;
-            btnListen.Text = "Listen";
-            btnListen.UseVisualStyleBackColor = true;
-            btnListen.Click += btnListen_Click;
             // 
             // comboBox1
             // 
@@ -72,25 +73,116 @@
             btnGetDevices.UseVisualStyleBackColor = true;
             btnGetDevices.Click += btnGetDevices_Click;
             // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(40, 134);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(sourceList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(btnExit);
+            splitContainer1.Panel2.Controls.Add(btnStop);
+            splitContainer1.Panel2.Controls.Add(btnStart);
+            splitContainer1.Panel2.Controls.Add(btnSources);
+            splitContainer1.Size = new Size(716, 287);
+            splitContainer1.SplitterDistance = 491;
+            splitContainer1.TabIndex = 4;
+            // 
+            // sourceList
+            // 
+            sourceList.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2 });
+            sourceList.Location = new Point(0, 0);
+            sourceList.MultiSelect = false;
+            sourceList.Name = "sourceList";
+            sourceList.Size = new Size(491, 287);
+            sourceList.TabIndex = 0;
+            sourceList.UseCompatibleStateImageBehavior = false;
+            sourceList.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Device";
+            columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Channels";
+            columnHeader2.Width = 160;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(8, 250);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(204, 23);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(8, 132);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(204, 23);
+            btnStop.TabIndex = 2;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(8, 103);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(204, 23);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnSources
+            // 
+            btnSources.Location = new Point(8, 15);
+            btnSources.Name = "btnSources";
+            btnSources.Size = new Size(204, 23);
+            btnSources.TabIndex = 0;
+            btnSources.Text = "Refresh Sources";
+            btnSources.UseVisualStyleBackColor = true;
+            btnSources.Click += btnSources_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
             Controls.Add(btnGetDevices);
             Controls.Add(comboBox1);
-            Controls.Add(btnListen);
             Controls.Add(btnPlay);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Audio Visualiser";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button btnPlay;
-        private Button btnListen;
         private ComboBox comboBox1;
         private Button btnGetDevices;
+        private SplitContainer splitContainer1;
+        private ListView sourceList;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private Button btnExit;
+        private Button btnStop;
+        private Button btnStart;
+        private Button btnSources;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,7 @@ using NAudio.Wave;
 namespace TheAudioVisualiser
 
 {
-    
+
     public partial class Form1 : Form
     {
         //string filePath = Application.StartupPath.;
@@ -14,6 +14,7 @@ namespace TheAudioVisualiser
         public Form1()
         {
             InitializeComponent();
+            sourceList.Items.Clear();
         }
 
         //Plays a specific .wav file from within the project: Pr0xima - Carrier Phreakuency.wav.
@@ -40,6 +41,70 @@ namespace TheAudioVisualiser
             }
         }
 
+        private void btnSources_Click(object sender, EventArgs e)
+        {
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<WaveInCapabilities>();
 
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+            }
+
+            //
+
+            foreach (var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                sourceList.Items.Add(item);
+            }
+        }
+
+        private NAudio.Wave.WaveIn sourceStream = null;
+        private NAudio.Wave.DirectSoundOut waveOut = null;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (sourceList.SelectedItems.Count == 0) return;
+
+            int deviceNumber = sourceList.SelectedItems[0].Index;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = deviceNumber;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(48000, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+            NAudio.Wave.WaveInProvider waveIn = new NAudio.Wave.WaveInProvider(sourceStream);
+
+            waveOut = new NAudio.Wave.DirectSoundOut();
+            waveOut.Init(waveIn);
+
+            sourceStream.StartRecording();
+            waveOut.Play();
+
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            btnStop_Click(sender, e);
+            this.Close();
+        }
     }
 }
